c1d15e7739a0ecb2cf054cfb2de860a0090952d4
Suraj Shende <suraj.shende@thecadre.in>
2025-05-16 10:08:21 +0530

    github action

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 // Define the API base URL
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://api-qa-dashboard.cadreodr.com";
 
 // Define types for test results
 interface Test {
